684b6b7cc29555b4fb4e445373d5e73047e9620e
Dmitrii Tiurin <dtiurin@neuro.net>
2024-09-01 17:26:59 +0300

    Перенос файлов в репозиторий с обучением

--- sql/sqltest_online_trainer/cte.sql
+++
@@ -1,9 +0,0 @@
---Напишите запрос возвращающий таблицу значений факториала для целых чисел от 0 до 10.
---Таблица должна содержать две колонки n - число от 0 до 10 и f значение факториала этого числа
-
-WITH RECURSIVE factorials AS (
-    SELECT 0 AS n, 1 AS f
-    UNION ALL
-    SELECT n + 1, (n + 1) * f FROM factorials WHERE n < 10
-)
-SELECT * FROM factorials;
